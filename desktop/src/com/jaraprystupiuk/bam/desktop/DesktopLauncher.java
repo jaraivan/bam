309afc0863e6b6109884b73138390f256d2258ea
jaraivan <ivanjaratamargo@gmail.com>
2018-08-19 23:29:52 -0300

    initial commit

---
+++ desktop/src/com/jaraprystupiuk/bam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jaraprystupiuk.bam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jaraprystupiuk.bam.Bam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bam(), config);
+	}
+}
